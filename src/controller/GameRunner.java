e4079427ccc31a2c5907a74c80642efbcdc2c5c5
Bmar21 <brandonmalor21@gmail.com>
2017-02-03 12:28:55 -0700

    Made the basic java files for the rough draft of my project

---
+++ src/controller/GameRunner.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class GameRunner 
+{
+	public static void main (String [] args)
+	{
+		PlayerController PlayerAppController = new PlayerController();
+		PlayerAppController.start();
+	}
+
+}
